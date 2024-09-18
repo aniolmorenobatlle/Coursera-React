8c37e6c6ed4f9a1337e65b7ba0990fa4bb3eca40
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2024-09-18 12:40:07 +0200

    Error correction

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,28 +2,6 @@ import RegisterForm from './Hooks2';
 
 import { useState } from 'react';
 
-// export default function InputComponent() { 
-//   const [inputText, setText] = useState('hello'); 
-
-//   function handleChange(e) { 
-//     setText(e.target.value); 
-//   } 
-
-//   return ( 
-//     <>
-
-//       <input value={inputText} onChange={handleChange} /> 
-
-//       <p>You typed: {inputText}</p>
-
-//       <button onClick={() => setText('hello')}> 
-//         Reset 
-//       </button>
-
-//     </> 
-//   ); 
-// } 
-
 function App() {
   const [inputText, setText] = useState('hello');
 
