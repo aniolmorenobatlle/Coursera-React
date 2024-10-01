8e0b1c8fef28cb04ad9aa9f8f448becfc69bc2b6
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2024-10-01 11:15:59 +0200

    Final Project Calculator

---
+++ links/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from 'react';
+import Home from './Home';
+import About from './About';
+
+import { Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
